9ce5a0ba27986c46bc459ea9cc382d0b1c4212e5
Andreas Backström <andreas@simplymozzo.se>
2022-02-06 17:22:42 +0100

    Add task service

---
+++ frontend/src/services/task.service.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import authHeader from "./auth-header";
+
+const API_URL = "/v1/";
+
+const getAllTasks = () => {
+    return axios.get(API_URL + "tasks", { headers: authHeader() });
+};
+
+const addTask = (name, list_id) => {
+  return axios.post(API_URL + "tasks", {
+    name,
+    list_id,
+  }, { headers: authHeader() });
+};
+
+
+const getTask = (id) => {
+  return axios.get(API_URL + "tasks/" + id, {}, { headers: authHeader() });
+};
+
+
+const exportedObject = {
+    getAllTasks,
+    getTask,
+    addTask,
+};
+
+export default exportedObject;
