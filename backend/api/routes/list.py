b74cba0b0677111e48e9f973386f27a9465aa150
Andreas Backström <mozzo242@gmail.com>
2022-02-06 20:11:11 +0100

    Require authentication for list routes

--- backend/api/routes/list.py
+++ backend/api/routes/list.py
@@ -1,28 +1,25 @@
 from flask import Blueprint, request, jsonify
-from models import List, ListSchema, db
+from models import List, ListSchema, db, User
+from flask_jwt_extended import (jwt_required, get_jwt_identity)
 
 list_endpoint = Blueprint('list', __name__)
 
 @list_endpoint.route("/v1/lists/<id>")
+@jwt_required()
 def get_list(id):
     list_schema = ListSchema(many=False)
-    list = List.query.get(id)
-    return jsonify(list_schema.dump(list))
-
-@list_endpoint.route("/v1/lists")
-def get_lists():
-    list_schema = ListSchema(many=True)
-    list = List.query.get(id)
+    list = List.query.filter_by(owner_id=User.find_by_email(get_jwt_identity()).id, id=id).first()
     return jsonify(list_schema.dump(list))
 
 @list_endpoint.route("/v1/lists", methods=["POST"])
+@jwt_required()
 def add_list():
     if not "name" in request.json or "project_id" not in request.json:
         return jsonify({
             "error": "Bad request",
             "message": "name and/or project_id not given"
         }), 400
-    new_list = List(name=request.json["name"], project_id=request.json["project_id"])
+    new_list = List(owner_id=User.find_by_email(get_jwt_identity()).id, name=request.json["name"], project_id=request.json["project_id"])
 
     new_list.save_to_db()
     return {
@@ -33,6 +30,7 @@ def add_list():
     }, 201
 
 @list_endpoint.route('/v1/lists/<id>', methods=["DELETE"])
+@jwt_required()
 def remove_list(id):
     list = List.query.get(id)
     try: 
