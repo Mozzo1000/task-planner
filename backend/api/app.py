f677acd7af0f4b2bbfcc80c34c5e47dbfc762e99
Andreas Backström <mozzo242@gmail.com>
2022-02-06 20:11:17 +0100

    Register list endpoint

--- backend/api/app.py
+++ backend/api/app.py
@@ -7,6 +7,7 @@ from models import db, ma
 from routes.auth import auth_endpoint
 from routes.task import task_endpoint
 from routes.project import project_endpoint
+from routes.list import list_endpoint
 
 swagger_template = {
   "swagger": "2.0",
@@ -28,6 +29,7 @@ jwt = JWTManager(app)
 app.register_blueprint(auth_endpoint)
 app.register_blueprint(task_endpoint)
 app.register_blueprint(project_endpoint)
+app.register_blueprint(list_endpoint)
 
 @app.route('/')
 def index():
