33dba97b03973d9698f669c6b3ac031cc8f3fa2d
Andreas Backström <mozzo242@gmail.com>
2022-02-06 19:19:22 +0100

    Add project service

---
+++ frontend/src/services/project.service.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import authHeader from "./auth-header";
+
+const API_URL = "/v1/";
+
+const getAll = () => {
+    return axios.get(API_URL + "projects", { headers: authHeader() });
+};
+
+const add = (name) => {
+  return axios.post(API_URL + "projects", {
+    name,
+  }, { headers: authHeader() });
+};
+
+
+const get = (id) => {
+  return axios.get(API_URL + "projects/" + id, { headers: authHeader() });
+};
+
+
+const exportedObject = {
+    getAll,
+    get,
+    add,
+};
+
+export default exportedObject;
