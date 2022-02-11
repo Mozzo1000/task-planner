eb1ead140314ea22c2c13de3cecd888eacae4a48
Andreas Backström <mozzo242@gmail.com>
2022-02-11 18:18:30 +0100

    Add get tasks in list to list.service

--- frontend/src/services/list.service.js
+++ frontend/src/services/list.service.js
@@ -19,11 +19,19 @@ const get = (id) => {
   return axios.get(API_URL + "lists/" + id, { headers: authHeader() });
 };
 
+const getTasksInList = (id, status) => {
+  if (status) {
+    return axios.get(API_URL + "lists/" + id + "/tasks?status=" + status, { headers: authHeader() });
+  } else {
+    return axios.get(API_URL + "lists/" + id + "/tasks", { headers: authHeader() });
+  }
+}
 
 const exportedObject = {
     getAll,
     get,
     add,
+    getTasksInList,
 };
 
 export default exportedObject;
