70b081f4f74448181f87100ab4a231effce8ffb2
Andreas Backström <mozzo242@gmail.com>
2022-02-11 19:59:03 +0100

    Add addTask

--- frontend/src/components/AddTask.js
+++ frontend/src/components/AddTask.js
@@ -5,12 +5,28 @@ import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import InputAdornment from '@mui/material/InputAdornment';
 import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
+import TaskService from '../services/task.service';
 
 function AddTask() {
-    const [value, setValue] = useState();
+    const [name, setName] = useState();
 
     const addTask = (e) => {
         e.preventDefault();
+        TaskService.addTask(name, null).then(
+            response => {
+                setName("");
+                console.log(response.data);
+            },
+            error => {
+                const resMessage = 
+                    (error.response &&
+                        error.response.data &&
+                        error.response.data.message) ||
+                    error.message ||
+                    error.toString();
+                console.log(resMessage);
+            }
+        )
     }
 
     return (
@@ -29,11 +45,12 @@ function AddTask() {
                     variant="standard"
                     autoFocus
                     fullWidth
-                    onChange={e => setValue(e.target.value)} 
+                    value={name}
+                    onChange={e => setName(e.target.value)} 
                 />
             </Grid>
             <Grid item xs={2}>
-                <Button variant="contained" startIcon={<AddIcon />} disabled={!value} fullWidth>Add</Button>
+                <Button variant="contained" startIcon={<AddIcon />} disabled={!name} onClick={addTask} fullWidth>Add</Button>
             </Grid>
         </Grid>
     )
