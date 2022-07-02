491977f019f38198df2a19b8b14504511cd631f3
Andreas Backström <mozzo242@gmail.com>
2022-07-02 19:42:09 +0200

    Change number into a badge in project list

--- frontend/src/components/ProjectListItem.js
+++ frontend/src/components/ProjectListItem.js
@@ -7,6 +7,43 @@ import Collapse from '@mui/material/Collapse';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import { Link } from "react-router-dom";
+import Typography from '@mui/material/Typography';
+import Badge from '@mui/material/Badge';
+import BadgeUnstyled, { badgeUnstyledClasses } from '@mui/base/BadgeUnstyled';
+import { styled } from '@mui/system';
+
+const StyledBadge = styled(BadgeUnstyled)`
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-size: 14px;
+    font-variant: tabular-nums;
+    list-style: none;
+    font-family: IBM Plex Sans, sans-serif;
+    position: relative;
+    display: inline-block;
+    line-height: 1;
+
+    & .${badgeUnstyledClasses.badge} {
+        z-index: auto;
+        min-width: 12px;
+        height: 20px;
+        padding: 0 6px;
+        color: #c1c6c9;
+        font-weight: bold;
+        font-size: 15px;
+        line-height: 20px;
+        white-space: nowrap;
+        text-align: center;
+        background: #f2f4f6;
+        border-radius: 5px;
+        position: absolute;
+        top: 0;
+        right: 15px;
+        transform: translate(50%, -50%);
+        transform-origin: 100% 0;
+    }
+`;
 
 function ProjectListItem(props) {
     const [open, setOpen] = useState(true);
@@ -17,7 +54,8 @@ function ProjectListItem(props) {
     return (
         <>
         <ListItemButton onClick={handleClick}>
-            <ListItemText primary={props.project.name + " (" + Object.keys(props.project.lists).length + ")"} />
+            <ListItemText primary={props.project.name} />
+            <StyledBadge badgeContent={Object.keys(props.project.lists).length} showZero></StyledBadge>
             {open ? <ExpandLess /> : <ExpandMore />}
         </ListItemButton>
         <Collapse in={open} timeout="auto" unmountOnExit>
