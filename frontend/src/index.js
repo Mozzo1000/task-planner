bbbc04cc330c4d081c9a29f275ccc759cf6b381c
Andreas Backström <mozzo242@gmail.com>
2022-07-03 10:13:31 +0200

    Add custom colors

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,27 @@ import './index.css';
 import App from './App';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+const { palette } = createTheme();
+const { augmentColor } = palette;
+const createColor = (mainColor) => augmentColor({ color: { main: mainColor } });
+const theme = createTheme({
+  palette: {
+    low: createColor('#ffeb3b'),
+    medium: createColor('#03a9f4'),
+    high: createColor('#ff9800'),
+    urgent: createColor('#f44336'),
+  },
+});
 
 
 ReactDOM.render(
   <React.StrictMode>
      <LocalizationProvider dateAdapter={AdapterDateFns}>
-       <App />
+       <ThemeProvider theme={theme}>
+        <App />
+       </ThemeProvider>
      </LocalizationProvider>
   </React.StrictMode>,
   document.getElementById('root')
