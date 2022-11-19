7a69fdf543ccaefec9e5e51e9c3ee7def897e02a
Andreas Backström <mozzo242@gmail.com>
2022-11-19 12:01:32 +0100

    Add dependency material-ui-snackbar-provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { SnackbarProvider } from 'material-ui-snackbar-provider'
 
 const container = document.getElementById("root");
 const root = createRoot(container);
@@ -26,7 +27,9 @@ root.render(
   <React.StrictMode>
     <LocalizationProvider dateAdapter={AdapterDateFns}>
       <ThemeProvider theme={theme}>
-        <App />
+        <SnackbarProvider SnackbarProps={{ autoHideDuration: 4000 }}>
+          <App />
+        </SnackbarProvider>
       </ThemeProvider>
     </LocalizationProvider>
   </React.StrictMode>
