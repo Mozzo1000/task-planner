23e8edc250175581122266ef015053b955d4a129
Andreas Backström <andreas@simplymozzo.se>
2022-12-03 17:57:05 +0100

    Return 400 if no data sent to edit task route

--- backend/api/routes/task.py
+++ backend/api/routes/task.py
@@ -5,10 +5,18 @@ from datetime import datetime
 
 task_endpoint = Blueprint('task', __name__)
 
+
 @task_endpoint.route("/v1/tasks/<id>", methods=["PATCH"])
 @jwt_required()
 def edit_task(id):
-    task = Task.query.filter_by(id=id, owner_id=User.find_by_email(get_jwt_identity()).id).first()
+    if not request.json:
+        return jsonify({
+            "error": "Bad request",
+            "message": "No data sent to server"
+        }), 400
+
+    task = Task.query.filter_by(
+        id=id, owner_id=User.find_by_email(get_jwt_identity()).id).first()
     if "name" in request.json:
         task.name = request.json["name"]
     if "description" in request.json:
@@ -25,6 +33,7 @@ def edit_task(id):
     task.save_to_db()
     return jsonify({'message': f'Task saved successfully'}), 200
 
+
 @task_endpoint.route("/v1/tasks")
 @jwt_required()
 def get_all_tasks():
@@ -32,14 +41,18 @@ def get_all_tasks():
     if request.args.get('include_done'):
         argument = request.args.get('include_done').lower()
         if argument == "false":
-            tasks = Task.query.filter(Task.owner_id==User.find_by_email(get_jwt_identity()).id, Task.status!="Done").order_by(Task.created_at.desc()).all()
+            tasks = Task.query.filter(Task.owner_id == User.find_by_email(
+                get_jwt_identity()).id, Task.status != "Done").order_by(Task.created_at.desc()).all()
         elif argument == "true":
-            tasks = Task.query.filter_by(owner_id=User.find_by_email(get_jwt_identity()).id).order_by(Task.created_at.desc()).all()
+            tasks = Task.query.filter_by(owner_id=User.find_by_email(
+                get_jwt_identity()).id).order_by(Task.created_at.desc()).all()
         else:
             return jsonify({'message': 'Invalid query'}), 401
     else:
-        tasks = Task.query.filter_by(owner_id=User.find_by_email(get_jwt_identity()).id).order_by(Task.created_at.desc()).all()
+        tasks = Task.query.filter_by(owner_id=User.find_by_email(
+            get_jwt_identity()).id).order_by(Task.created_at.desc()).all()
     return jsonify(task_schema.dump(tasks))
+
 
 @task_endpoint.route("/v1/tasks/stats")
 @jwt_required()
@@ -47,22 +60,29 @@ def get_task_stats():
     from_date = request.args.get("from", default="1900-01-01")
     to_date = request.args.get("to", default=datetime.now())
     if request.args:
-        tasks_completed = Task.query.filter(Task.owner_id==User.find_by_email(get_jwt_identity()).id, Task.status=="Done", Task.created_at.between(from_date, to_date)).count()
-        tasks_not_done = Task.query.filter(Task.owner_id==User.find_by_email(get_jwt_identity()).id, Task.status!="Done", Task.created_at.between(from_date, to_date)).count()
+        tasks_completed = Task.query.filter(Task.owner_id == User.find_by_email(get_jwt_identity(
+        )).id, Task.status == "Done", Task.created_at.between(from_date, to_date)).count()
+        tasks_not_done = Task.query.filter(Task.owner_id == User.find_by_email(get_jwt_identity(
+        )).id, Task.status != "Done", Task.created_at.between(from_date, to_date)).count()
     else:
-        tasks_completed = Task.query.filter(Task.owner_id==User.find_by_email(get_jwt_identity()).id, Task.status=="Done").count()
-        tasks_not_done = Task.query.filter(Task.owner_id==User.find_by_email(get_jwt_identity()).id, Task.status!="Done").count()
+        tasks_completed = Task.query.filter(Task.owner_id == User.find_by_email(
+            get_jwt_identity()).id, Task.status == "Done").count()
+        tasks_not_done = Task.query.filter(Task.owner_id == User.find_by_email(
+            get_jwt_identity()).id, Task.status != "Done").count()
     return jsonify({
-            "completed": tasks_completed,
-            "not_done": tasks_not_done
-        }), 201
+        "completed": tasks_completed,
+        "not_done": tasks_not_done
+    }), 201
+
 
 @task_endpoint.route("/v1/tasks/<id>")
 @jwt_required()
 def get_task(id):
     task_schema = TaskSchema(many=False)
-    task = Task.query.filter_by(owner_id=User.find_by_email(get_jwt_identity()).id, id=id).first()
+    task = Task.query.filter_by(owner_id=User.find_by_email(
+        get_jwt_identity()).id, id=id).first()
     return jsonify(task_schema.dump(task))
+
 
 @task_endpoint.route("/v1/tasks", methods=["POST"])
 @jwt_required()
@@ -82,8 +102,9 @@ def add_task():
         due_date = request.json["due_date"]
     else:
         due_date = None
-    
-    new_task = Task(name=request.json["name"], list_id=request.json["list_id"], description=description, due_date=due_date, owner_id=User.find_by_email(get_jwt_identity()).id)
+
+    new_task = Task(name=request.json["name"], list_id=request.json["list_id"], description=description,
+                    due_date=due_date, owner_id=User.find_by_email(get_jwt_identity()).id)
 
     new_task.save_to_db()
     return {
@@ -93,11 +114,13 @@ def add_task():
         "created_at": new_task.created_at
     }, 201
 
+
 @task_endpoint.route('/v1/tasks/<id>', methods=["DELETE"])
 @jwt_required()
 def remove_task(id):
-    task = Task.query.filter_by(owner_id=User.find_by_email(get_jwt_identity()).id, id=id).first()
-    try: 
+    task = Task.query.filter_by(owner_id=User.find_by_email(
+        get_jwt_identity()).id, id=id).first()
+    try:
         db.session.delete(task)
         db.session.commit()
         return jsonify({'message': f'Task {id} successfully removed'}), 200
